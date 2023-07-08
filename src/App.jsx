8de4f91b35aa32f004e79b7fac886685d8afa1af
Kumari Jaya <jaya28032001@gmail.com>
2023-07-09 00:16:14 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/*import './App.css'
+import { useState } from 'react';
 
 function App() {
+
+  // eslint-disable-next-line no-unused-vars
+  const [count, setCount] = useState(0)
   
+  const handleClick = () => {
+    console.log("Button clicked");
+    if(count>0){
+ setCount(count-1)
+    }
+   
+  };
 
   return (
-    <>
-      <div>
-        <h1>Hello world</h1>
-       </div>
-    </>
-  )
+    <div>
+      <p>{count}</p>
+    <button onClick={handleClick}>-</button>
+    </div>
+  );
 }
 
 export default App
+*/
+
+import './App.css'
+
+function App(){
+  const home = () => {
+    console.log("Button clicked");
+   
+  };
+  const about = () => {
+    console.log("Button clicked");
+   
+  };
+  const contact = () => {
+    console.log("Button clicked");
+   
+  };
+  return(
+    
+    <div>
+    <button className="my-button" onClick={home}>Home</button>
+    <button className="my-button" onClick={about}>About</button>
+    <button className="my-button" onClick={contact}>Contact Us</button>
+
+    </div>
+  )
+}
+
+export default App;
